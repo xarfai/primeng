7828211c530bf1b394f7073145c6e5d9b8c55e0d
Gerd Broeker <gerd.b@idlepeak.com>
2018-03-29 08:57:02 +0200

    5.2.4-next-1

--- src/app/components/contextmenu/contextmenu.ts
+++ src/app/components/contextmenu/contextmenu.ts
@@ -12,7 +12,8 @@ import {RouterModule} from '@angular/router';
             <ng-template ngFor let-child [ngForOf]="(root ? item : item.items)">
                 <li *ngIf="child.separator" class="ui-menu-separator ui-widget-content">
                 <li *ngIf="!child.separator" #item [ngClass]="{'ui-menuitem ui-corner-all':true,'ui-menuitem-active':item==activeItem}"
-                    (mouseenter)="onItemMouseEnter($event,item,child)" (mouseleave)="onItemMouseLeave($event,item)">
+                    (mouseenter)="onItemMouseEnter($event,item,child)" (mouseleave)="onItemMouseLeave($event,item)" 
+                    [style.display]="child.visible === false ? 'none' : 'block'">
                     <a *ngIf="!child.routerLink" [href]="child.url||'#'" [attr.target]="child.target" [attr.title]="child.title" [attr.id]="child.id" (click)="itemClick($event, child)"
                         [ngClass]="{'ui-menuitem-link ui-corner-all':true,'ui-state-disabled':child.disabled}" [ngStyle]="child.style" [class]="child.styleClass">
                         <span class="ui-submenu-icon fa fa-fw fa-caret-right" *ngIf="child.items"></span>
@@ -37,17 +38,17 @@ import {RouterModule} from '@angular/router';
 export class ContextMenuSub {
 
     @Input() item: MenuItem;
-    
+
     @Input() root: boolean;
-    
+
     constructor(public domHandler: DomHandler, @Inject(forwardRef(() => ContextMenu)) public contextMenu: ContextMenu) {}
-        
+
     activeItem: any;
 
     containerLeft: any;
 
     hideTimeout: any;
-                
+
     onItemMouseEnter(event, item, menuitem) {
         if(menuitem.disabled) {
             return;
@@ -57,7 +58,7 @@ export class ContextMenuSub {
             clearTimeout(this.hideTimeout);
             this.hideTimeout = null;
         }
-        
+
         this.activeItem = item;
         let nextElement =  item.children[0].nextElementSibling;
         if(nextElement) {
@@ -66,35 +67,35 @@ export class ContextMenuSub {
             this.position(sublist, item);
         }
     }
-    
+
     onItemMouseLeave(event, link) {
         this.hideTimeout = setTimeout(() => {
             this.activeItem = null;
         }, 1000);
     }
-    
+
     itemClick(event, item: MenuItem) {
         if(item.disabled) {
             event.preventDefault();
             return;
         }
-        
+
         if(!item.url) {
             event.preventDefault();
         }
-        
-        if(item.command) {            
+
+        if(item.command) {
             item.command({
                 originalEvent: event,
                 item: item
             });
         }
     }
-    
+
     listClick(event) {
         this.activeItem = null;
     }
-    
+
     position(sublist, item) {
         this.containerLeft = this.domHandler.getOffset(item.parentElement)
         let viewport = this.domHandler.getViewport();
@@ -118,7 +119,7 @@ export class ContextMenuSub {
 
         let scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
         document.body.removeChild(scrollDiv);
-        
+
         return scrollbarWidth;
     }
 }
@@ -136,27 +137,27 @@ export class ContextMenuSub {
 export class ContextMenu implements AfterViewInit,OnDestroy {
 
     @Input() model: MenuItem[];
-    
+
     @Input() global: boolean;
-    
+
     @Input() target: any;
 
     @Input() style: any;
 
     @Input() styleClass: string;
-    
+
     @Input() appendTo: any;
 
     @Input() autoZIndex: boolean = true;
-    
+
     @Input() baseZIndex: number = 0;
-    
+
     @ViewChild('container') containerViewChild: ElementRef;
-                    
+
     documentClickListener: any;
-    
+
     rightClickListener: any;
-        
+
     constructor(public el: ElementRef, public domHandler: DomHandler, public renderer: Renderer2) {}
 
     ngAfterViewInit() {
@@ -173,7 +174,7 @@ export class ContextMenu implements AfterViewInit,OnDestroy {
                 event.stopPropagation();
             });
         }
-        
+
         if(this.appendTo) {
             if(this.appendTo === 'body')
                 document.body.appendChild( this.containerViewChild.nativeElement);
@@ -181,19 +182,19 @@ export class ContextMenu implements AfterViewInit,OnDestroy {
                 this.domHandler.appendChild(this.containerViewChild.nativeElement, this.appendTo);
         }
     }
-        
+
     show(event?: MouseEvent) {
         this.position(event);
         this.moveOnTop();
         this.containerViewChild.nativeElement.style.display = 'block';
         this.domHandler.fadeIn(this.containerViewChild.nativeElement, 250);
         this.bindDocumentClickListener();
-        
+
         if(event) {
             event.preventDefault();
         }
     }
-    
+
     hide() {
         this.containerViewChild.nativeElement.style.display = 'none';
         this.unbindDocumentClickListener();
@@ -204,14 +205,14 @@ export class ContextMenu implements AfterViewInit,OnDestroy {
             this.containerViewChild.nativeElement.style.zIndex = String(this.baseZIndex + (++DomHandler.zindex));
         }
     }
-    
+
     toggle(event?: MouseEvent) {
         if(this.containerViewChild.nativeElement.offsetParent)
             this.hide();
         else
             this.show(event);
     }
-    
+
     position(event?: MouseEvent) {
         if(event) {
             let left = event.pageX + 1;
@@ -219,32 +220,32 @@ export class ContextMenu implements AfterViewInit,OnDestroy {
             let width = this.containerViewChild.nativeElement.offsetParent ? this.containerViewChild.nativeElement.offsetWidth: this.domHandler.getHiddenElementOuterWidth(this.containerViewChild.nativeElement);
             let height = this.containerViewChild.nativeElement.offsetParent ? this.containerViewChild.nativeElement.offsetHeight: this.domHandler.getHiddenElementOuterHeight(this.containerViewChild.nativeElement);
             let viewport = this.domHandler.getViewport();
-            
+
             //flip
             if(left + width - document.body.scrollLeft > viewport.width) {
                 left -= width;
             }
-            
+
             //flip
             if(top + height - document.body.scrollTop > viewport.height) {
                 top -= height;
             }
-            
+
             //fit
             if(left < document.body.scrollLeft) {
                 left = document.body.scrollLeft;
             }
-            
+
             //fit
             if(top < document.body.scrollTop) {
                 top = document.body.scrollTop;
             }
-                
+
             this.containerViewChild.nativeElement.style.left = left + 'px';
             this.containerViewChild.nativeElement.style.top = top + 'px';
         }
     }
-    
+
     bindDocumentClickListener() {
         if(!this.documentClickListener) {
             this.documentClickListener = this.renderer.listen('document', 'click', (event) => {
@@ -254,21 +255,21 @@ export class ContextMenu implements AfterViewInit,OnDestroy {
             });
         }
     }
-    
+
     unbindDocumentClickListener() {
         if(this.documentClickListener) {
             this.documentClickListener();
             this.documentClickListener = null;
         }
     }
-        
+
     ngOnDestroy() {
         this.unbindDocumentClickListener();
-        
+
         if(this.rightClickListener) {
             this.rightClickListener();
         }
-                
+
         if(this.appendTo) {
             this.el.nativeElement.appendChild(this.containerViewChild.nativeElement);
         }
